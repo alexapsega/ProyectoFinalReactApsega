6fb9ebe4e9fbd947a74f0dc98bb9cff55b68e4cf
Alejo Apsega <alejoapsega@gmail.com>
2024-01-03 16:55:02 -0300

    Carga Vite Project

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <div>CartWidget</div>
+  )
+}
+
+export default CartWidget
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+
+const NavBar = () => {
+  return (
+    <div>
+      <p>Book E-commerce</p>
+      <CartWidget />
+    </div>
+  )
+}
+
+export default NavBar
